edfb1b0775ac8b26dfb1f4bf24f92debbe89e9e0
Michał Wiśniewski <istius1@gmail.com>
2025-10-15 03:12:05 +0200

    implement py-person-class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    Person.people = {}
+    persons_list = [Person(person_dict["name"],
+                    person_dict["age"]) for person_dict
+                    in people if person_dict.get("name")
+                    and person_dict.get("age") is not None]
+
+    for person_dict in people:
+        current_person = Person.people.get(person_dict.get("name"))
+        if not current_person:
+            continue
+        partner_name = person_dict.get("wife")
+        if partner_name and partner_name in Person.people:
+            current_person.wife = Person.people[partner_name]
+        partner_name = person_dict.get("husband")
+        if partner_name and partner_name in Person.people:
+            current_person.husband = Person.people[partner_name]
+    return persons_list
